cc12dbedd4309ccccea9bed4c5710a9ed04822a2
王少丽 <9684199+wangshaoli1214@user.noreply.gitee.com>
2024-09-27 13:59:33 +0800

    调整深色模式页面范围

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <van-config-provider theme="dark">
     <RouterView />
-  </van-config-provider>
 </template>
 
 <style scoped>
--- src/views/welfare/welfareIndex.vue
+++ src/views/welfare/welfareIndex.vue
@@ -3,8 +3,16 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-    <RouterView />
+  <van-config-provider theme="dark">
+    <div class="welfare-index">
+        <RouterView />
+    </div>
+  </van-config-provider>
 </template>
 
 <style scoped>
+.welfare-index {
+    background-color: #073936;
+    height: 100vh;
+}
 </style>
