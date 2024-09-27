7b0a07098e68c1f6b56467727b8627304c32d2f2
imooring <1143242432@qq.com>
2024-09-27 13:38:43 +0800

    list

--- src/views/recruit/list.vue
+++ src/views/recruit/list.vue
@@ -1,6 +1,12 @@
 
 
 <template>
+    <van-nav-bar title="招聘列表" fixed :border="false" left-arrow left-text="返回" @click-left="onClickLeft">
+        <template #right>
+            <van-icon name="search" size="18" />
+        </template>
+    </van-nav-bar>
+
     <div>11111list</div>
 </template>
 <script setup>
