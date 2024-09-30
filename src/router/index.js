6dc5498e93e0fefbfd2858f7cec75f913361e2b2
王少丽 <9684199+wangshaoli1214@user.noreply.gitee.com>
2024-09-30 16:00:53 +0800

    我的项目路由

--- src/router/index.js
+++ src/router/index.js
@@ -55,6 +55,11 @@ const router = createRouter({
           component: () => import('../views/recruit/detail.vue')
         }
       ]
+    },
+    {
+      path: 'myProject',
+      name:'myProject',
+      component: () => import('../views/myProject/index.vue')
     }
   ]
 })
