30499e0846a7a972478a246905059cd257179b24
imooring <1143242432@qq.com>
2024-09-30 17:07:10 +0800

    店铺列表

--- src/request/api.js
+++ src/request/api.js
@@ -2,4 +2,5 @@ import request from '../utils/request/';
 
 export default {
     recruitList: (params) => request.get('/api/yibum/post/page', { ...params }),
+    shopList: (params) => request.get('/api/yibum/basicInformation/shopBranch/page', { ...params }),
 }
