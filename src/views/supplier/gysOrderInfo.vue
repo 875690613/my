21f6e802a2cde3ca3b47652911cc5781e7cb0183
陈琦 <2528529768@qq.com>
2024-12-13 15:29:59 +0800

    代码提交

--- src/views/supplier/gysOrderInfo.vue
+++ src/views/supplier/gysOrderInfo.vue
@@ -62,11 +62,13 @@ export default {
         </div>
         <div style="border: 1px solid #BBBBBB;margin-top: 10px;" v-for="(item, index) in dzList" :key="index">
             <div style="display: flex;padding: 10px;">
-                <div style="display: flex;width: 50%;">
+                <div style="display: flex;width: 100%;">
                     <div style="color: #6C6C6C;flex-shrink: 0;">采购订单号：</div>
                     <div style="color: #000000;word-break: break-all;">{{ item.PurchaseNo }}</div>
                 </div>
-                <div style="display: flex;width: 50%;">
+            </div>
+            <div style="display: flex;padding: 10px;">
+                <div style="display: flex;width: 100%;">
                     <div style="color: #6C6C6C;flex-shrink: 0;">采购类型：</div>
                     <div style="color: #000000;word-break: break-all;">{{ item.PurchaseType }}</div>
                 </div>
@@ -74,89 +76,91 @@ export default {
             <div style="text-align: center;color: #BEBEBE;border-bottom: 1px solid #BBBBBB;">采购明细</div>
             <div style="padding: 10px 10px 10px 20px;">
                 <div style="display: flex;margin-bottom: 10px;">
-                    <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">物料编号：</div>
+                    <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">物料编号：</div>
                     <div style="color: #000000;word-break: break-all;">{{ item.StockNo }}</div>
                 </div>
                 <div style="display: flex;margin-bottom: 10px;">
-                    <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">物料品名：</div>
+                    <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">物料品名：</div>
                     <div style="color: #000000;word-break: break-all;">{{ item.StockName }}</div>
                 </div>
                 <div style="display: flex;margin-bottom: 10px;">
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">颜色：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">颜色：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.Color }}</div>
                     </div>
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">型号：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">型号：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.Specification }}</div>
                     </div>
                 </div>
                 <div style="display: flex;margin-bottom: 10px;">
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">采购单价：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">采购单价：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.UnitPrice }}</div>
                     </div>
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">币别：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">币别：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.Currencies }}</div>
                     </div>
                 </div>
                 <div style="display: flex;margin-bottom: 10px;">
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">采购数量：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">采购数量：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.PlanNumber }}</div>
                     </div>
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">计量单位：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">计量单位：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.Measure }}</div>
                     </div>
                 </div>
                 <div style="display: flex;margin-bottom: 10px;">
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">小缸费：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">小缸费：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.Vat }}</div>
                     </div>
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">上机费：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">上机费：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.Machine }}</div>
                     </div>
                 </div>
                 <div style="display: flex;margin-bottom: 10px;">
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">网费：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">网费：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.Net }}</div>
                     </div>
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">印花费：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">印花费：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.Printing }}</div>
                     </div>
                 </div>
                 <div style="display: flex;margin-bottom: 30px;">
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">其他费用：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">其他费用：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.OtherFee }}</div>
                     </div>
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">总价：</div>
+                        <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">总价：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.TotalPrice }}</div>
                     </div>
                 </div>
                 <div style="display: flex;align-items: center;">
-                    <div style="width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">订单款号：</div>
+                    <div style="min-width: 78px;text-align: right;color: #6C6C6C;flex-shrink: 0;">订单款号：</div>
                     <div style="color: #000000;">
                         <div>{{ item.StyleNos }}</div>
                     </div>
                 </div>
             </div>
             <div style="text-align: center;color: #BEBEBE;border-bottom: 1px solid #BBBBBB;padding-bottom: 10px;">物流进度</div>
-            <div style="padding: 10px 0;">
+            <div style="padding: 10px 0 10px 10px;">
                 <div style="display: flex;margin-bottom: 10px;">
-                    <div style="display: flex;width: 50%;">
-                        <div style="width: 105px;text-align: right;color: #6C6C6C;flex-shrink: 0;">计划到货日期：</div>
+                    <div style="display: flex;width: 100%;">
+                        <div style="min-width: 105px;text-align: right;color: #6C6C6C;flex-shrink: 0;">计划到货日期：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.EstimatedArriveDate }}</div>
                     </div>
-                    <div style="display: flex;width: 50%;">
-                        <div style="width: 105px;text-align: right;color: #6C6C6C;flex-shrink: 0;">实际到货日期：</div>
+                </div>
+                <div style="display: flex;margin-bottom: 10px;">
+                    <div style="display: flex;width: 100%;">
+                        <div style="min-width: 105px;text-align: right;color: #6C6C6C;flex-shrink: 0;">实际到货日期：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.ArriveDate }}</div>
                     </div>
                 </div>
@@ -169,46 +173,46 @@ export default {
             <div style="padding: 10px;">
                 <div style="display: flex;">
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">门幅：</div>
+                        <div style="min-width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">门幅：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.TheWidth }}</div>
                     </div>
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">克重：</div>
+                        <div style="min-width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">克重：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.TheWeight }}</div>
                     </div>
                 </div>
                 <div style="display: flex;">
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">干磨等级：</div>
+                        <div style="min-width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">干磨等级：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.DryLevle }}</div>
                     </div>
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">湿磨等级：</div>
+                        <div style="min-width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">湿磨等级：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.WetLevel }}</div>
                     </div>
                 </div>
                 <div style="display: flex;">
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">涤沾色等级：</div>
+                        <div style="min-width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">涤沾色等级：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.PolyColorLevel }}</div>
                     </div>
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">棉沾色等级：</div>
+                        <div style="min-width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">棉沾色等级：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.CottonColorLevle }}</div>
                     </div>
                 </div>
                 <div style="display: flex;">
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">经向缩率：</div>
+                        <div style="min-width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">经向缩率：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.TwistWarp }}</div>
                     </div>
                     <div style="display: flex;width: 50%;">
-                        <div style="width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">纬向缩率：</div>
+                        <div style="min-width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">纬向缩率：</div>
                         <div style="color: #000000;word-break: break-all;">{{ item.TwistWeft }}</div>
                     </div>
                 </div>
                 <div style="display: flex;margin-bottom: 10px;">
-                    <div style="width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">测试结果：</div>
+                    <div style="min-width: 90px;text-align: right;color: #6C6C6C;flex-shrink: 0;">测试结果：</div>
                     <div style="color: #000000;word-break: break-all;">{{ item.TestResult }}</div>
                 </div>
             </div>
